98e79cfb26f2dc168214d7bc0a4f482dc1aefb1d
Wallysson Bruno Araujo Monteiro <wallyssonunb@gmail.com>
2025-11-12 11:43:57 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,14 @@
+let LUZ = 0
 input.onButtonPressed(Button.A, function () {
+    LUZ = 0
     led.plotBrightness(2, 2, randint(0, 250))
+    LUZ = led.pointBrightness(2, 2)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(LUZ / 250 * 100)
+    basic.showString("%")
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    if (led.pointBrightness(2, 2) * (100 / 250) < 25) {
-        basic.showNumber(led.pointBrightness(0, 0) * (100 / 250))
-        basic.showString("%")
-        basic.clearScreen()
-    } else if (led.pointBrightness(2, 2) * (100 / 250) <= 50) {
-        basic.showNumber(led.pointBrightness(2, 2) * (100 / 250))
-        basic.showString("%")
-        basic.clearScreen()
-    } else if (led.pointBrightness(2, 2) * (100 / 250) <= 75) {
-        basic.showNumber(led.pointBrightness(2, 2) * (100 / 250))
-        basic.showString("%")
-        basic.clearScreen()
-    } else {
-        basic.showNumber(led.pointBrightness(2, 2) * (100 / 250))
-        basic.showString("%")
-        basic.clearScreen()
-    }
+    basic.showNumber(LUZ)
 })
